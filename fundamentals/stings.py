7a0945556efc0858fc81b86c8469bdd92573a87b
Michael Stiles <mstiles01@icloud.com>
2020-02-25 15:30:45 -0600

    basic string lesson

---
+++ fundamentals/stings.py
@@ -0,0 +1,17 @@
+# Variable for Giraffe Academy
+phrase = "Giraffe Academy"
+
+#Converting phrase to upper and checking if upper
+print(phrase.upper().isupper())
+
+#Checking Length
+print(len (phrase))
+
+#giving me the character for inedx 3
+print(phrase[3])
+
+#Telling me where the Word "Acad" Starts
+print(phrase.index("Acad"))
+
+#Replaces the word with another word
+print(phrase.replace("Giraffe", "Elephant"))
