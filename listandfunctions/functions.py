bff46ae140e8542a204609e4d428c9fc2a75d2a1
Michael Stiles <mstiles01@icloud.com>
2020-02-26 09:26:57 -0600

    very basic of functions

---
+++ listandfunctions/functions.py
@@ -0,0 +1,13 @@
+# def is the name for functions in python
+# After def sayhi(): all code after : is in the function
+# Code for function needs to be indented
+# Anything not indented is not considered in the function by python
+def sayhi(name):
+    print("Hello " + name)
+
+#Showing flow
+print("Top")
+sayhi("Michael")
+print("bottom")
+sayhi("Bryce")
+
