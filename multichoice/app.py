ef6d40074d8098449553221cbf4969823f234ac2
Michael Stiles <mstiles01@icloud.com>
2020-04-28 20:45:58 -0500

    Question promps

---
+++ multichoice/app.py
@@ -0,0 +1,5 @@
+question_prompts = [
+"What color are Apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n"
+"What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n"
+"What color are Strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
