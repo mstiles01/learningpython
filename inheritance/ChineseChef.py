d4681b14604f7fcaac2c10d923095ebc53de1160
Michael Stiles <mstiles01@icloud.com>
2020-05-05 19:42:17 -0500

    basic inheritance

---
+++ inheritance/ChineseChef.py
@@ -0,0 +1,10 @@
+from Chef import Chef
+
+class ChineseChef(Chef):
+
+    def make_special_dish(self):
+        print("The chef makes a Orange Chicken")
+
+    def make_fired_rice(self):
+        print("The chef makes fried rice")
+
